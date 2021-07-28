e804892cbd9ff7287ad454ae061c0f4d359b74c6
cgrountree <cgrountree@gmail.com>
2021-07-27 21:02:26 -0400

    add debug logs everywhere

--- src/main/java/com/dorksquad/artwork/artwork/ArtworkService.java
+++ src/main/java/com/dorksquad/artwork/artwork/ArtworkService.java
@@ -23,26 +23,34 @@ public class ArtworkService {
 
 
     public String addArtwork(String name, String album, MultipartFile image) throws IOException {
+        logger.debug("ArtworkService: addArtwork was called. Calling MongoRepository to insert artwork...");
         Binary binaryFile = new Binary(BsonBinarySubType.BINARY, image.getBytes());
         Artwork artwork = new Artwork(name, album, binaryFile);
         artworkRepo.insert(artwork);
-        logger.debug("add artwork was called");
         return artwork.getName();
     }
 
     public Artwork getArtworkByName(String name) {
+        logger.debug(String.format("ArtworkService: getArtworkByName was called. " +
+                "Calling MongoRepository to find an artwork with name='%s'...", name));
         return artworkRepo.findByName(name);
     }
 
     public List<Artwork> getArtworks() {
+        logger.debug("ArtworkService: getArtworks with not sort was called. " +
+                "Calling MongoRepository to get artworks...");
         return artworkRepo.findAll();
     }
 
     public List<Artwork> getArtworks(String sort) {
+        logger.debug(String.format("ArtworkService: getArtworks with sort='%s' was called. " +
+                "Calling MongoRepository to get sorted artworks...", sort));
         return artworkRepo.findAll(Sort.by(sort).ascending());
     }
 
     public Artwork getArtworkByAlbum(String album) {
+        logger.debug(String.format("ArtworkService: getArtworkByAlbum was called. " +
+                "Calling MongoRepository to find an artwork with album='%s'...", album));
         return artworkRepo.findByAlbum(album);
     }
 }
